c4eea59e8a896b7373f46a006d4264faa0dddc4a
FIAX <axentefilip1@gmail.com>
2025-05-07 13:42:06 +0300

    Update ReadDH11.py

--- Temperature Sensor/DH11/Code/ReadDH11.py
+++ Temperature Sensor/DH11/Code/ReadDH11.py
@@ -2,11 +2,20 @@ from machine import Pin
 from dht import DHT11  # Import DHT11 class
 from time import sleep
 
+from sys import stdout
+
+
 sensor = DHT11(Pin(4))  # Set up DHT11 sensor on GPIO4
 
-while True:
-    sensor.measure()  # Take a measurement
-    temp = sensor.temperature  # Access the property without ()
-    humidity = sensor.humidity  # Access the property without ()
-    print(f"Temperature: {temp}°C, Humidity: {humidity}%")
-    sleep(1)
+def run2 ():
+    global temp, humidity  # Ensure they are accessible across the script
+    while True:
+        sensor.measure()  # Take a measurement
+        temp = sensor.temperature  # Access the property without ()
+        humidity = sensor.humidity  # Access the property without ()
+        print(f"Temperature: {temp}°C, Humidity: {humidity}%")
+        
+        stdout.write(f"{temp},{humidity}\n")
+        stdout.flush()  # Ensure the data is sent
+        
+        sleep(5)
